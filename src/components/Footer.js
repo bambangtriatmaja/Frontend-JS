fcea222f3d58436c8dee15bdbd21e41e388a3468
bambang-ti <triatmajabambang375@gmail.com>
2022-03-15 22:52:29 +0700

    First commit React App movie-database

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+/**
+ * Membuat component footer
+ * Component footer menampung content footer
+ * Menggunakan arrow function.
+ */
+const Footer = () => {
+  return (
+    <footer>
+      <h2>Copyright @bams</h2>
+      <p>Created by reactJS</p>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+/**
+ * Membuat component header.
+ * Menampilkan navigasi.
+ * Menggunakan arrow function
+ */
+const Header = () => {
+  return (
+    <nav>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>contact</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Header;
