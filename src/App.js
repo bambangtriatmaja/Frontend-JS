bcf42d139bd22898bd2a7936f4435d131d2073e1
bambang-ti <triatmajabambang375@gmail.com>
2022-03-31 14:58:59 +0700

    task pertemuan 6

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Counter from "./components/Counter/Counter";
 import Home from "./pages/Home";
 
 function App() {
   return (
     <div>
       <Home />
+      {/* <Counter /> */}
     </div>
   );
 }
--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,84 +1,35 @@
+import Movie from "../Movie/Movie";
 import styles from "./Movies.module.css";
+import data from "../utils/constants/data";
+import { useState } from "react";
+import { nanoid } from "nanoid";
 
 function Movies() {
+  // membuat variable state:movies
+  const [movies, setMovies] = useState(data);
+
+  function tambahFilm() {
+    const movie = {
+      id: nanoid(),
+      title: "Naruto",
+      year: "2022",
+      type: "Series",
+      poster: "https://picsum.photos/300/400",
+    };
+
+    setMovies([...movies, movie]);
+  }
+
   return (
     <div className={styles.container}>
       <section className={styles.movies}>
         <h2 className={styles.movies__title}>Latest Movies</h2>
         <div className={styles.movie__container}>
-          <div className={styles.movie}>
-            <img
-              className={styles.movie__image}
-              src="https://picsum.photos/300/400"
-              alt=""
-            />
-            <h3 className={styles.movie__title}>Title Movie</h3>
-            <p className={styles.movie__date}>2022</p>
-          </div>
-          <div className={styles.movie}>
-            <img
-              className={styles.movie__image}
-              src="https://picsum.photos/300/400"
-              alt=""
-            />
-            <h3 className={styles.movie__title}>Title Movie</h3>
-            <p className={styles.movie__date}>2022</p>
-          </div>
-          <div className={styles.movie}>
-            <img
-              className={styles.movie__image}
-              src="https://picsum.photos/300/400"
-              alt=""
-            />
-            <h3 className={styles.movie__title}>Title Movie</h3>
-            <p className={styles.movie__date}>2022</p>
-          </div>
-          <div className={styles.movie}>
-            <img
-              className={styles.movie__image}
-              src="https://picsum.photos/300/400"
-              alt=""
-            />
-            <h3 className={styles.movie__title}>Title Movie</h3>
-            <p className={styles.movie__date}>2022</p>
-          </div>
-          <div className={styles.movie}>
-            <img
-              className={styles.movie__image}
-              src="https://picsum.photos/300/400"
-              alt=""
-            />
-            <h3 className={styles.movie__title}>Title Movie</h3>
-            <p className={styles.movie__date}>2022</p>
-          </div>
-          <div className={styles.movie}>
-            <img
-              className={styles.movie__image}
-              src="https://picsum.photos/300/400"
-              alt=""
-            />
-            <h3 className={styles.movie__title}>Title Movie</h3>
-            <p className={styles.movie__date}>2022</p>
-          </div>
-          <div className={styles.movie}>
-            <img
-              className={styles.movie__image}
-              src="https://picsum.photos/300/400"
-              alt=""
-            />
-            <h3 className={styles.movie__title}>Title Movie</h3>
-            <p className={styles.movie__date}>2022</p>
-          </div>
-          <div className={styles.movie}>
-            <img
-              className={styles.movie__image}
-              src="https://picsum.photos/300/400"
-              alt=""
-            />
-            <h3 className={styles.movie__title}>Title Movie</h3>
-            <p className={styles.movie__date}>2022</p>
-          </div>
+          {movies.map(function (movie) {
+            return <Movie key={movie.id} movie={movie} />;
+          })}
         </div>
+        <button onClick={tambahFilm}>Add Movie</button>
       </section>
     </div>
   );
